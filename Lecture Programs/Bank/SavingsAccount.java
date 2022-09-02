bdf9a133a1b0d378f6589ea84eb5ea905e7c5373
Lurk390 <elbasiouny.mahmoud@gmail.com>
2022-09-02 15:31:02 -0400

    Added withdraw function and getter

--- Lecture Programs/Bank/SavingsAccount.java
+++ Lecture Programs/Bank/SavingsAccount.java
@@ -2,15 +2,34 @@ package Bank;
 
 class SavingsAccount extends BankAccount {
     private double maxWithdrawals;
-    private double withdrawalsPerMonth;
+    private double numWithdraws;
 
-    SavingsAccount(String firstName, String lastName, String 
-                    accountNumber, double balance, double 
-                    interestRate, double maxWithdrawals, double 
-                    withdrawalsPerMonth) {
+    SavingsAccount(String firstName, String lastName, 
+                   String accountNumber, double balance, double interestRate,
+                   double maxWithdrawals, double numWithdraws) {
         super(firstName, lastName, accountNumber, balance,
               interestRate);
         this.maxWithdrawals = maxWithdrawals;
-        this.withdrawalsPerMonth = withdrawalsPerMonth;
+        this.numWithdraws = numWithdraws;
+    }
+
+    public boolean withdraw(double amount) {
+        if (numWithdraws < maxWithdrawals) {
+            boolean success = super.withdraw(amount);
+            if (success) {
+                numWithdraws++;
+            }
+            return success; 
+        } else {
+            return false;
+        }
+    }
+
+    public String toString() {
+        return "Savings: " + super.toString();
+    }
+
+    public double getNumWithdraws() {
+        return numWithdraws;
     }
 }
