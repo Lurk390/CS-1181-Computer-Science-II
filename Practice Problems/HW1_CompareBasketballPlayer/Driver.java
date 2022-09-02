a307629708749d97f10be3f5769818c3fa26f759
Lurk390 <elbasiouny.mahmoud@gmail.com>
2022-09-02 15:43:54 -0400

    Removed directions

--- Practice Problems/HW1_CompareBasketballPlayer/Driver.java
+++ Practice Problems/HW1_CompareBasketballPlayer/Driver.java
@@ -29,28 +29,3 @@ public class Driver {
         System.out.println(playerList);
     }
 }
-
-/*
-Your task is to write a class called BasketballPlayer. Your class should have the following fields and
-methods:
-private String firstName
-private String lastName
-private double ppg (points per game)
-public BasketballPlayer – constructor that iniFalizes the fields of this object
-public String toString – returns a String of the form lastName, firstName (ppg)
-
-Your BasketballPlayer class also needs to implement the Comparable interface so that basketball players
-can be compared to one another. Players should be sorted first by points per game (higher comes first),
-then lexicographically by last name (ignoring case) and finally lexicographically by first name.
-
-You also need to create a class called Driver.java with a main method. In main:
-Create three BasketballPlayer objects
-Put the player objects into an ArrayList
-Print out the ArrayList
-Sort the ArrayList
-Print out the ArrayList a second Fme
-
-Example output:
-[Smith, Jay (12.7), Smith, DeShaun (12.7), Taylor, DeShaun (18.1)]
-[Taylor, DeShaun (18.1), Smith, DeShaun (12.7), Smith, Jay (12.7)]
- */
--- Practice Problems/HW2_ShoppingTrip/Driver.java
+++ Practice Problems/HW2_ShoppingTrip/Driver.java
@@ -23,37 +23,3 @@ public class Driver {
         System.out.println("Total: $" + Math.round(total * 100) / 100.0);
     }
 }
-
-/*
-Directions:
-The total cost of a group of items at a grocery store is based on the sum of the 
-individual product prices and the tax (which is 5.75%). Products that are considered 
-“necessities” are not taxed, whereas products that are considered “luxuries” are.
-
-For this practice problem you will need to download ShoppingTripStartingCode.zip
-from Pilot. 
-
-The Product class is abstract, and it has a method called getTotalPrice. Your task is 
-to create two subclasses of Product: NecessaryProduct and LuxuryProduct and 
-implement the getTotalPrice method in each of these classes appropriately. Then 
-modify the driver program to instantiate four products (two necessary and two 
-luxury) and store them in the product array, print out each item in the array, and 
-display the total cost of the items.
-
-You should not make any changes at all to Product.java, and you should only add to 
-ShoppingTripStartingCode.java. Do not change any code that is already present.
-
-Example:
-(Cheese and bread are necessities and soda and candy are luxuries)
-Cheese $1.50
-Soda $3.50
-Bread $2.25
-Candy $2.00
-The total cost is $9.57
-
-The computation is 1.50 + (3.50 * 1.0575) + 2.25 + (2.00 * 1.0575) = 9.57. Note that 
-the price displayed next to each product is the price without tax.
-
-Keep in mind that to receive credit your code needs to work for all valid inputs, not 
-just for the particular example shown above.
- */
