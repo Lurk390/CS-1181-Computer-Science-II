9790d13c39707d1240d49f4f90b04e6c3df9506e
Lurk390 <elbasiouny.mahmoud@gmail.com>
2022-09-11 16:33:19 -0400

    Initial upload

---
+++ Practice Problems/HW3_ExceptionalDivision/ExceptionalDivision.java
@@ -0,0 +1,44 @@
+// Mahmoud Elbasiouny
+
+package HW3_ExceptionalDivision;
+import java.util.Scanner;
+
+public class ExceptionalDivision {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int numerator = 0;
+        int denominator = 0;
+
+        while (true) {
+            try {
+                System.out.print("What is the numerator? ");
+                numerator = input.nextInt();
+                break;
+            } catch (Exception InputMismatchException) {
+                System.out.println("Error: Please enter a whole number.");
+                input.nextLine();
+            }
+        }
+
+        while (true) {
+            try {
+                System.out.print("What is the denominator? ");
+                denominator = input.nextInt();
+                
+                if (denominator == 0) {
+                    System.out.println("Error. Can not divide by zero.");
+                    input.nextLine();
+                } else {
+                    break;
+                }
+            } catch (Exception InputMismatchException) {
+                System.out.println("Error: Please enter a whole number.");
+                input.nextLine();
+            }
+        }
+        input.close();
+
+        int quotient = numerator / denominator;
+        System.out.println(numerator + " / " + denominator + " = " + quotient);
+    }
+}
