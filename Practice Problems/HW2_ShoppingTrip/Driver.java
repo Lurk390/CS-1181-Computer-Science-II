6e44438659d2bae585d815673fe05dc1b701d1ff
Lurk390 <elbasiouny.mahmoud@gmail.com>
2022-09-19 03:02:28 -0400

    Changed to different products and tax rate

--- Practice Problems/HW2_ShoppingTrip/Driver.java
+++ Practice Problems/HW2_ShoppingTrip/Driver.java
@@ -7,10 +7,10 @@ public class Driver {
         Product[] products = new Product[4];
 
         // Create two necessary and two luxury products and add them to the products array
-        products[0] = new NecessaryProduct("Milk", 2.99, 0.07);
-        products[1] = new NecessaryProduct("Bread", 3.99, 0.07);
-        products[2] = new LuxuryProduct("iPhone", 999.99);
-        products[3] = new LuxuryProduct("MacBook", 1999.99);
+        products[0] = new NecessaryProduct("Cheese", 1.50);
+        products[1] = new LuxuryProduct("Soda", 3.50, 0.0575);
+        products[2] = new NecessaryProduct("Bread", 2.25);
+        products[3] = new LuxuryProduct("Candy", 2.00, 0.0575);
 
         // Print each product and its price and calculate the total price
         double total = 0;
