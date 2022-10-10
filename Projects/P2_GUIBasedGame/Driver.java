bbdd2404f7cb89decafd7ae5bc628e297f90d1a2
Lurk390 <elbasiouny.mahmoud@gmail.com>
2022-10-10 02:01:44 -0400

    Initial upload

--- Projects/P2_GUIBasedGame/Driver.java
+++ Projects/P2_GUIBasedGame/Driver.java
@@ -1,3 +1,4 @@
+// Mahmoud Elbasiouny
 package P2_GUIBasedGame;
 
 import javax.swing.*;
@@ -9,17 +10,24 @@ public class Driver extends JFrame {
         JPanel root = new JPanel();
         Instructions iPanel = new Instructions();
         Game gPanel = new Game();
+        Score sPanel = new Score();
         
         root.setLayout(new BorderLayout());
-        root.add(gPanel , BorderLayout.CENTER);
+        root.add(gPanel, BorderLayout.CENTER);
         root.add(iPanel, BorderLayout.NORTH);
+        root.add(sPanel, BorderLayout.SOUTH);
+        
+        iPanel.setBackground(Color.gray);
+        gPanel.setBackground(Color.gray);
+        sPanel.setBackground(Color.gray);
 
         this.getContentPane().add(root);
-        this.setSize(500,400);
+        this.setTitle("Catch the Fruit!");
+        this.setSize(1000,800);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
-        
+
         gPanel.setFocusable(true);
         gPanel.requestFocusInWindow();
     }
--- Projects/P2_GUIBasedGame/Instructions.java
+++ Projects/P2_GUIBasedGame/Instructions.java
@@ -1,28 +1,56 @@
+// Mahmoud Elbasiouny
 package P2_GUIBasedGame;
 
+import java.awt.*;
 import javax.swing.*;
 
 public class Instructions extends JPanel {
     
     public Instructions() {
         JPanel root = new JPanel();
-        JButton instructionsB = new JButton("Instructions");
-        instructionsB.addActionListener(e1 -> {
-            JFrame popUp = new JFrame();
-            JLabel message = new JLabel("asdfasfa");
-            JButton close = new JButton("Close");
-            instructionsB.addActionListener(e2 -> {
-                
-            });
+        JPanel instructions = new JPanel();
+        
+        JLabel message = new JLabel("How To Play");
+        JLabel message2 = new JLabel(
+            " 1. Use the left and right arrow keys to move the plate."
+        );
+        JLabel message3 = new JLabel(
+            " 2. Use A and D to move the plate even faster!"
+        );
+        JLabel message4 = new JLabel(
+            " 3. The goal is to catch the falling fruit."
+        );
+        JLabel message5 = new JLabel(
+            " 4. If you miss 3 fruit, you lose."
+        );
+        JLabel message6 = new JLabel(
+            " 5. If you catch 10 fruit, you win."
+        );
+        JLabel message7 = new JLabel(
+            " 6. Good luck!"
+        );
 
-            popUp.getContentPane().add(message);
+        root.setLayout(new BoxLayout(root, BoxLayout.Y_AXIS));
+        root.add(message);
+        root.add(instructions);
 
-            popUp.setVisible(true);
-            popUp.setSize(500,400);
-            popUp.setLocationRelativeTo(null);
-        });
+        instructions.setLayout(new GridLayout(3,2));
+        instructions.add(message2);
+        instructions.add(message3);
+        instructions.add(message4);
+        instructions.add(message5);
+        instructions.add(message6);
+        instructions.add(message7);
 
-        root.add(instructionsB);
+        // Set message one to center
+        message.setHorizontalAlignment(JLabel.CENTER);
+
+        root.setVisible(true);
+        root.setSize(400,200);
+
+        instructions.setBackground(Color.gray);
+        root.setBackground(Color.gray);
         this.add(root);
     }
 }
+
