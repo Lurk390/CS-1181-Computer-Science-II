37d680b55898fa64127f54184238c769d7a55ba6
Mahmoud Elbasiouny <elbasiouny.mahmoud@gmail.com>
2022-10-14 12:08:55 -0400

    Initial upload

---
+++ Practice Problems/HW6_LinkedLists/Node.java
@@ -0,0 +1,33 @@
+// Mahmoud Elbasiouny
+package HW6_LinkedLists;
+
+public class Node {
+    private int element;
+    private Node next;
+    
+    public Node(int element) {
+        this.element = element;
+        next = null;
+    }
+    
+    public int getElement() {
+        return element;
+    }
+    
+    public void setElement(int element) {
+        this.element = element;
+    }
+    
+    public Node getNext() {
+        return next;
+    }
+    
+    public void setNext(Node next) {
+        this.next = next;
+    }
+    
+    @Override
+    public String toString() {
+        return "" + element;
+    }
+}
