d3f2b0261a3bc54cb5aea688232350ac96b3422e
Lurk390 <elbasiouny.mahmoud@gmail.com>
2022-10-20 16:20:16 -0400

    Added removeN function

--- Practice Problems/HW6_LinkedLists/LinkedList.java
+++ Practice Problems/HW6_LinkedLists/LinkedList.java
@@ -30,7 +30,35 @@ public class LinkedList {
     public int getSize() {
         return size;
     }
-   
+
+    public boolean removeN(int element, int n) {
+        Node current = head;
+        int count = 0;
+
+        while (current != null) {
+            if (current.getElement() == element) {
+                count++;
+            }
+            current = current.getNext();
+        }
+        if (count < n) {
+            return false;
+        }
+        current = head;
+        while (current != null) {
+            if (current.getElement() == element) {
+                current.setElement(current.getNext().getElement());
+                current.setNext(current.getNext().getNext());
+                n--;
+            }
+            if (n == 0) {
+                break;
+            }
+            current = current.getNext();
+        }
+        return true;
+    }
+
     @Override
     public String toString() {
         String s = "";
