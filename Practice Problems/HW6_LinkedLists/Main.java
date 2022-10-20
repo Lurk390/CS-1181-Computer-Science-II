b57dc18fbb5461b4884c4132d9309c5fa5985ea5
Lurk390 <elbasiouny.mahmoud@gmail.com>
2022-10-20 16:20:37 -0400

    Added test cases to test removeN function

--- Practice Problems/HW6_LinkedLists/Main.java
+++ Practice Problems/HW6_LinkedLists/Main.java
@@ -4,9 +4,6 @@ package HW6_LinkedLists;
 class Main {
 
   public static void main(String[] args) {
-    /*
-    theChain is 3, 4, 7, 6, 3, 2, 9, 6, 3, 6
-    */
     LinkedList list = new LinkedList();
     list.add(3);
     list.add(4);
@@ -18,7 +15,17 @@ class Main {
     list.add(6);
     list.add(3);
     list.add(6);
-
     System.out.println(list);
+    // 3, 4, 7, 6, 3, 2, 9, 6, 3, 6
+
+    list.removeN(6, 2);
+    System.out.println(list);
+    // 3, 4, 7, 3, 2, 9, 3, 6
+    // 6 was removed 2 times starting from the beginning of the list
+
+    list.removeN(3, 4);
+    System.out.println(list);
+    // 3, 4, 7, 3, 2, 9, 3, 6
+    // No change as there are only 2 occurrences of 3 in the list
   }
 }
